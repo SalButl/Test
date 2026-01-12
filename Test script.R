65e541304f67c6ed52db43708dc4dafda74ddd73
SalButl <sally.butler1@health.nsw.gov.au>
2026-01-13 09:07:23 +1100

    Filter less than 30

--- Test script.R
+++ Test script.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(here)
 
 test <- gss_cat %>% 
-  filter(age >= 30) %>% 
+  filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
