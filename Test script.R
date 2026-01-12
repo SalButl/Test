9732f6e240cb089b2bee42e9201499772a79ae35
SalButl <sally.butler1@health.nsw.gov.au>
2026-01-13 09:18:24 +1100

    Change name

--- Test script.R
+++ Test script.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(here)
 
-test <- gss_cat %>% 
+tv_hours_tab <- gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
